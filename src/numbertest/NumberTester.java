cdc0bd60fe1e81c9c58dcd039f9d74359a93e914
datboivasi <vasi.maglov@outlook.com>
2019-10-03 18:09:49 +0200

    expantion

--- src/numbertest/NumberTester.java
+++ src/numbertest/NumberTester.java
@@ -5,10 +5,51 @@
  */
 package numbertest;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 /**
  *
- * @author vasim
+ * @author vmaglov17
  */
 public class NumberTester {
+   
+    public final String fileName;
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+    
+    
+    public interface NumberTest {
+        boolean testNumber(int number);
+    }
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+
+    
+    public void setOddEvenTester(NumberTest oddTester) {
+       this.oddTester = oddTester;
+    }
+
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        
+        
+    }
+
+    
+    
     
 }
